82864c8b6f9edbd7da6a2683a08dfcb72344255d
Max Schulze <max@maxschulze.com>
2010-12-11 15:31:10 +0100

    changed require 'md5' to require 'digest/md5' for ruby 1.9

--- lib/capitate/plugins/prompt.rb
+++ lib/capitate/plugins/prompt.rb
@@ -1,4 +1,4 @@
-require 'md5'
+require 'digest/md5'
 
 module Capitate::Plugins::Prompt
 
@@ -11,7 +11,7 @@ module Capitate::Plugins::Prompt
   def ask(label, options = {}, &block)
     Capistrano::CLI.ui.ask(label, &block)
   end
-  
+
   # Prompt for password.
   #
   # ==== Options (options)
@@ -25,15 +25,15 @@ module Capitate::Plugins::Prompt
   # :max_attempts<Integer>:: Number of attempts to retry. _Defaults to 3_
   #
   def password(label, options = {})
-    
+
     verify = options[:verify]
     lazy = options[:lazy].nil? ? true : options[:lazy]
     check_hash = options[:check_hash]
     max_attempts = options[:max_attempts] || 3
-    
+
     # Lazy
-    password_prompt = Proc.new { 
-      
+    password_prompt = Proc.new {
+
       attempts = 0
       password = nil
       success = true
@@ -41,38 +41,38 @@ module Capitate::Plugins::Prompt
       loop do
         password = Capistrano::CLI.password_prompt(label)
         attempts += 1
-    
+
         if verify
           password_verify = Capistrano::CLI.password_prompt("[VERIFY] #{label}")
           if password != password_verify
-            logger.important "Passwords do not match" 
+            logger.important "Passwords do not match"
             success = false
           end
         end
-      
+
         if check_hash
-          if MD5.md5(password).hexdigest != check_hash
-            logger.important "Invalid password, try again." 
-            success = false            
-          end         
+          if Digest::MD5.md5(password).hexdigest != check_hash
+            logger.important "Invalid password, try again."
+            success = false
+          end
         end
-        
+
         break if success
         break if attempts >= max_attempts
-        
+
         # Reset success
         success = true
       end
-      
+
       raise "Invalid password, too many tries" unless success
-    
+
       password
     }
-    
+
     return password_prompt if lazy
     password_prompt.call
-  end  
-  
+  end
+
   # Preview variables, and re-set them if asked to.
   # Uses highline menus. Is not very intelligent.
   #
@@ -83,27 +83,27 @@ module Capitate::Plugins::Prompt
   #
   def preview_variables(variables, header = "Verify your settings", prompt = "Choose: ")
     hl = Capistrano::CLI.ui
-    
+
     confirmed = false
-    while not confirmed do 
+    while not confirmed do
       hl.choose do |menu|
         menu.header = header
         menu.prompt = prompt
-        
+
         menu.choice("<Continue>") do
           confirmed = true
         end
-        
+
         variables.each do |variable|
           menu.choice("#{variable}: #{fetch(variable)}") do
             new_setting = ask("Set #{variable}: ") { |q| q.default = fetch(variable) }
             set variable, new_setting
           end
-        end        
+        end
       end
     end
   end
-  
+
 end
 
 Capistrano.plugin :prompt, Capitate::Plugins::Prompt
